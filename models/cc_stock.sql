95cc8dce9bfd02dde3b81cadb3011d8a0d6b1399
Nicolas <glaus.nicolas@gmail.com>
2023-08-02 11:16:40 +0000

    staging stock

--- models/cc_stock.sql
+++ models/cc_stock.sql
@@ -2,26 +2,8 @@ WITH cc_sales_products AS (
     SELECT * FROM {{ ref('cc_sales_products')}}
 ),
 
-stg_cc_stock AS (
-  SELECT
-    ### Key ###
-    CONCAT(model,"_",color,"_",IFNULL(size,"no-size")) AS product_id 
-    ###########
-    ,model
-    ,color
-    ,size
-    -- name
-    ,model_name
-    ,color_name
-    ,CONCAT(model_name," ",color_name,IF(size IS NULL,"",CONCAT(" - Taille ",size))) AS product_name
-    -- product info --
-    ,t.new AS pdt_new
-    -- stock metrics --
-    ,forecast_stock
-    ,stock
-    -- value
-    ,price
-  FROM `raw_data_circle.raw_cc_stock` t
+WITH stg_cc_stock AS (
+    SELECT *FROM {{ ref('stg_cc_stock')}}
 )
 
 SELECT
---
+++ models/stg_cc_sales.sql
@@ -0,0 +1,7 @@
+SELECT
+  date_date
+  ### Key ###  
+  ,product_id
+  ###########
+  ,quantity AS qty
+FROM `raw_data_circle.raw_cc_sales`
